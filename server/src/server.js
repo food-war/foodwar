ae712bcca5307ff15111282a536eb322ed9c7b5a
백호동 <ghehd2311@gmail.com>
2019-11-02 17:32:41 +0900

    MongoDb Connect

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,14 @@
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
-import env from './config/env'
+import { BACKEND_PORT, BACKEND_MONGODB} from './config/env'
 
 import mongoose from 'mongoose'
 
 /* Routes */
 import userRouter from './routes/api/userRouter'
 
-const APP_PORT = env.BACKEND_PORT;
+const APP_PORT = BACKEND_PORT;
 
 const app = express();
 
@@ -20,6 +20,16 @@ app.use('/api/user', userRouter);
 app.get('/sayHello', function(req, res) {
   res.send('Hello from the foodwar back-end!!!');
 });
+
+/**
+ * @database    mongoose
+ * @desc        Foodwar database
+ * @access      Public
+ */
+mongoose.Promise = global.Promise;
+mongoose.connect(BACKEND_MONGODB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
+    .then(() => console.log('MongoDb Connected..'))
+    .catch(err => console.log(err));
 
 app.listen(APP_PORT);
 console.log('Webserver listening to port', APP_PORT);
