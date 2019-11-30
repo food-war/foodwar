d6ec8aeb0f7e851c1817d4274c025f4a0305648d
백호동 <ghehd2311@gmail.com>
2019-11-30 20:58:04 +0900

    로그인 axios 이용해서 서버단과 연결

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,8 +1,43 @@
-import { TEST_DISPATCH, LOGIN_ACTION } from './types';
+import { TEST_DISPATCH, LOGIN_ACTION, GET_ERRORS } from './types';
+import axios from 'axios';
+import { REACT_APP_BACKEND_API_URL, REACT_APP_LOCAL_URL } from '../config/env';
 
+const nowUrl = window.location.href;
 // LOGIN
-export const loginUser = userData => {
-  console.log(`userData ${userData.email} ${userData.password}`);
+export const loginUser = userData => dispatch => {
+  //console.log(`userData ${userData.email} ${userData.password}`);
+  let requestUrl = REACT_APP_LOCAL_URL;
+  if (nowUrl.indexOf('localhost') === -1) {
+    requestUrl = REACT_APP_BACKEND_API_URL;
+  }
+  var headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+  };
+  // 백엔드 api/user/login로 비동기 요청 보내기
+  axios
+    .post(`${requestUrl}/api/user/login`, userData, headers)
+    .then(res => {
+      //console.log(res.data);
+      const { success, token } = res.data;
+      console.log(success, token);
+      dispatch({ type: LOGIN_ACTION });
+    })
+    .catch(err => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err,
+      });
+    });
+  //테스트 코드 :
+  //   axios({
+  //     method: 'post',
+  //     url: `${requestUrl}/api/user/login`,
+  //     data: {
+  //       email: userData.email,
+  //       password: userData.password,
+  //     },
+  //   });
+
   return {
     type: LOGIN_ACTION,
     payload: userData,
--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,5 +1,5 @@
-import gravatar from 'gravatar'
-import bcrypt from 'bcryptjs'
+import gravatar from 'gravatar';
+import bcrypt from 'bcryptjs';
 
 const userModel = require('../models/userModel');
 
@@ -12,116 +12,110 @@ const keys = require('../config/keys');
 const validateRegisterInput = require('../validation/registerValidation');
 const validateLoginInput = require('../validation/loginValidation');
 
-
 module.exports = {
-    test: async (req, res) => {
-        res.status(200).json({message: "User Works!"})
-    },
+  test: async (req, res) => {
+    res.status(200).json({ message: 'User Works!' });
+  },
 
-    /**
+  /**
      * @controller  POST api/user/register
      * @desc        user register
      * @access      Public
      */
-    register: async (req, res) => {
-        const {errors, isValid} = validateRegisterInput(req.body);
+  register: async (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
 
-        if(!isValid){
-            return res.status(400).json(errors);
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+    userModel
+      .findOne({ email: req.body.email })
+      .then(user => {
+        if (user) {
+          // return res.status(400).json({message: "This email already exists."});
+          errors.email = 'This email already exists.';
+          return res.status(400).json(errors);
         }
-        userModel
-            .findOne({email: req.body.email})
-            .then(user => {
-                
-                if(user){
-                    // return res.status(400).json({message: "This email already exists."});
-                    errors.email = "This email already exists.";
-                    return res.status(400).json(errors);
-                }
 
-                /** Create new avatar */
-                const avatar = gravatar.url(req.body.email, {
-                    s: '200',
-                    r: 'pg',
-                    d: 'mm'
-                });
+        /** Create new avatar */
+        const avatar = gravatar.url(req.body.email, {
+          s: '200',
+          r: 'pg',
+          d: 'mm',
+        });
 
-                /** Create New User */
-                const newUser = new userModel({
-                    name: req.body.name,
-                    email: req.body.email,
-                    avatar,
-                    password: req.body.password
-                });
+        /** Create New User */
+        const newUser = new userModel({
+          name: req.body.name,
+          email: req.body.email,
+          avatar,
+          password: req.body.password,
+        });
 
-                /** After password encryption, sign up */
-                bcrypt.genSalt(10, (err, salt) => {
-                    bcrypt.hash(newUser.password, salt, (err,hash) => {
-                        if(err) throw err;
-                        newUser.password = hash;
-                        newUser.save()
-                            .then(user => res.status(200).json(user))
-                            .catch(err => res.status(404).json(err));
-                    });
-                });
-            })
-            .catch(err => res.status(404).json(err));
-    }, //END Register
-    /**
+        /** After password encryption, sign up */
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => res.status(200).json(user))
+              .catch(err => res.status(404).json(err));
+          });
+        });
+      })
+      .catch(err => res.status(404).json(err));
+  }, //END Register
+  /**
      * @controller  POST api/user/login
      * @desc        user login
      * @access      Public
      */
-    login: async (req, res) => {
-        
-        const {errors, isValid} = validateLoginInput(req.body);
-        
-        if(!isValid){
-            res.status(400).json(errors);
+  login: async (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    if (!isValid) {
+      res.status(400).json(errors);
+    }
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    /** Find users by email */
+    userModel
+      .findOne({ email })
+      .then(user => {
+        if (!user) {
+          errors.email = 'Users not found';
+          return res.status(400).json(errors);
         }
 
-        const email = req.body.email;
-        const password = req.body.password;
-
-        /** Find users by email */
-        userModel.findOne({email})
-            .then(user => {
-                if(!user){
-                    errors.email = 'Users not found';
-                    return res.status(400).json(errors);
-                }
-
-                bcrypt
-                    .compare(password, user.password)
-                    .then(isMatch => {
-                        if(isMatch){
-                            const payload = {id: user.id, name: user.name, avatar: user.avatar};
-                            // Sign Token
-                            jwt.sign(
-                                payload,
-                                keys.secretOrKey,
-                                {expiresIn: 28800},
-                                (err, token) => {
-                                    res.json({
-                                        success: true,
-                                        token: 'Bearer ' + token
-                                    })
-                                }
-                            )
-                        }else {
-                            errors.password = 'Password incorrect';
-                            return res.status(400).json(errors);
-                        }
-                    })
-
-            })
-            .catch(err => res.status(404).json(err));
-    },//END LOGIN
-    current: async (req, res ) => {
-        res.json({
-            id: req.user.id,
-            name: req.user.name,
-            email: req.user.email
+        bcrypt.compare(password, user.password).then(isMatch => {
+          //   console.log(
+          //     `password : ${password} user.passwrod: ${user.password} isMatch = ${isMatch}`,
+          //   );
+          if (isMatch) {
+            const payload = { id: user.id, name: user.name, avatar: user.avatar };
+            // Sign Token
+            jwt.sign(payload, keys.secretOrKey, { expiresIn: 28800 }, (err, token) => {
+              res.json({
+                success: true,
+                token: 'Bearer ' + token,
+              });
+            });
+          } else {
+            errors.password = 'Password incorrect';
+            return res.status(400).json(errors);
+          }
         });
-    }
-} 
+      })
+      .catch(err => res.status(404).json(err));
+  }, //END LOGIN
+  current: async (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email,
+    });
+  },
+};
