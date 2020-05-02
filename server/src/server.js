13bb7ca4ab6fa019532954d6a51d7de34d092c84
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-05-02 22:27:42 +0900

    test

--- server/src/server.js
+++ server/src/server.js
@@ -58,7 +58,7 @@ mongoose
   })
   .then(() => console.log('MongoDB Connected...'))
   .catch((err) => console.log(err));
-// mongoose.set('userFindAndModify', false);
+mongoose.set('userFindAndModify', false);
 /** mongoose 관련 코드 끝 */
 
 /**user routes */
