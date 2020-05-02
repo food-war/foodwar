a7bf0d5d80a1be93bfc5a6f074cf9a77cc3ee8f0
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-05-02 22:30:30 +0900

    빌드 간소화

---
+++ client/build2/precache-manifest.41f5592731267f3e56a91525fce314a5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82a6aa402644d8069c8f8cfeeb0200b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "469bc04a0ce74b3ec64d",
+    "url": "/static/css/0.b3a817e8.chunk.css"
+  },
+  {
+    "revision": "40b0c3ba0899c59dcdd5",
+    "url": "/static/css/4.4ee76db1.chunk.css"
+  },
+  {
+    "revision": "64b3b4f812d542b076ce",
+    "url": "/static/css/5.ece195d0.chunk.css"
+  },
+  {
+    "revision": "48db990c731148fbdabb",
+    "url": "/static/css/6.0b01ea3f.chunk.css"
+  },
+  {
+    "revision": "1efb36d9bdc12709d993",
+    "url": "/static/css/main.b34f3978.chunk.css"
+  },
+  {
+    "revision": "469bc04a0ce74b3ec64d",
+    "url": "/static/js/0.ce5db37c.chunk.js"
+  },
+  {
+    "revision": "7d8f148db7baad2f1b9f",
+    "url": "/static/js/3.c1ae295c.chunk.js"
+  },
+  {
+    "revision": "40b0c3ba0899c59dcdd5",
+    "url": "/static/js/4.aa233c8a.chunk.js"
+  },
+  {
+    "revision": "64b3b4f812d542b076ce",
+    "url": "/static/js/5.786cb777.chunk.js"
+  },
+  {
+    "revision": "48db990c731148fbdabb",
+    "url": "/static/js/6.ea6b773d.chunk.js"
+  },
+  {
+    "revision": "83a79a6d4571f6319629",
+    "url": "/static/js/7.9782a0b4.chunk.js"
+  },
+  {
+    "revision": "1efb36d9bdc12709d993",
+    "url": "/static/js/main.229a044b.chunk.js"
+  },
+  {
+    "revision": "14fd0f4ee7aacd8af143",
+    "url": "/static/js/runtime-main.df65dcd0.js"
+  }
+]);
